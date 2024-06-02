f4c7c8a21c2acf211c6deee87e6099cbde02eaba
Anantha Bharatarajan <ananth.bharatarajan@gmail.com>
2024-06-02 19:46:22 +1000

    Exporting orders functionality initiated

--- src/BurritoKing_A2/AllOrdersController.java
+++ src/BurritoKing_A2/AllOrdersController.java
@@ -1,5 +1,7 @@
 package BurritoKing_A2;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -9,10 +11,13 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 //This class shows the user all of their orders, irrespective of the order status
 public class AllOrdersController implements Initializable
@@ -38,11 +43,56 @@ public class AllOrdersController implements Initializable
 	@FXML
 	public TableView allOrdersTable;
 	
+	@FXML
+	public Button exportOrdersButton;
+	
 	//Opening the dashboard page
 	public void openDashboardPage(ActionEvent event) throws IOException 
 	{
 		pages.dashboardPage(event);
 	}
+	
+	public void exportAllOrders(ActionEvent event) 
+	{
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Orders");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("CSV Files", "*.csv"));
+
+        Stage stage = (Stage) exportOrdersButton.getScene().getWindow();
+        File file = fileChooser.showSaveDialog(stage);
+
+        if (file != null) 
+        {
+            saveOrdersToFile(file);
+        }
+    }
+
+    private void saveOrdersToFile(File file) 
+    {
+        ObservableList<OrderClass> orders = Database.getAllOrders();
+
+        try (FileWriter writer = new FileWriter(file)) 
+        {
+            // Write CSV header
+            writer.write("Order ID,Total Cost,Order Status,Ordered Items\n");
+
+            for (OrderClass order : orders) 
+            {
+                writer.write(order.getOrderID().get() + "," +
+                             order.getOrderTotalCost().get() + "," +
+                             order.getOrderStatus().get() + "," +
+                             order.getOrderAllItems().get() + "\n");
+            }
+        } 
+        catch (IOException e) 
+        {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("An error occurred while saving the file.");
+            alert.showAndWait();
+        }
+    }
 	
 	//Initializing the table to show all orders
 	@Override
